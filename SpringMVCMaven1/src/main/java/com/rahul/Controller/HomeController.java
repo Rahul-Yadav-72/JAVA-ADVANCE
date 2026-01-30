ce9619be84ce36259bb5ea503baf7185ef143ebe
Rahul Kumar Yadav <ry642499@gmail.com>
2026-01-30 11:08:52 +0530

    Update HomeController logic

--- SpringMVCMaven1/src/main/java/com/rahul/Controller/HomeController.java
+++ SpringMVCMaven1/src/main/java/com/rahul/Controller/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
-	public String home(Model model) {
-		model.addAttribute("msg", "Spring MVC maven demo");
-		return "home";
-	}
 
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("msg", "Spring MVC Maven is Working!");
+        return "home";
+    }
 }
